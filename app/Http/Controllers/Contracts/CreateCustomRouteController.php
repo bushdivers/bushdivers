c097a96747c52ed36bd1d0ec0aeba7aa83b86ec6
Russell West <russellwwest@gmail.com>
2021-12-17 09:24:04 +0000

    Base for custom route creation

---
+++ app/Http/Controllers/Contracts/CreateCustomRouteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Contracts;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CreateCustomRouteController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
---
+++ app/Services/Contracts/CreateCustomRoute.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services\Contracts;
+
+class CreateCustomRoute
+{
+    public function execute($dep, $arr)
+    {
+
+    }
+}
