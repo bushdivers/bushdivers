ab5928a7b6988b16915aa08c845e791efcd4a21c
Russell West <russellwwest@gmail.com>
2021-12-28 21:58:02 +0000

    additional scenario for testing bearing

--- tests/Unit/Services/Airport/AirportBearingTest.php
+++ tests/Unit/Services/Airport/AirportBearingTest.php
@@ -19,4 +19,11 @@ class AirportBearingTest extends TestCase
         $heading = $calcBearing->execute(-6.36188, 143.23070, -5.82781, 144.29953, 5.51567);
         $this->assertEquals(57, $heading);
     }
+
+    public function test_bearing_between_ayfo_and_aymr()
+    {
+        $calcBearing = $this->app->make(CalcBearingBetweenPoints::class);
+        $heading = $calcBearing->execute(-6.50917, 143.07904, -6.36188, 143.23070, 5.33706);
+        $this->assertEquals(39, $heading);
+    }
 }
