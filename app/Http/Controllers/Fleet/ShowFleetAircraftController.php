8c9cdd9f8131870437bae2767dbba8423b6b62d2
Russell West <russellwwest@gmail.com>
2022-01-05 09:52:26 +0000

    Hides scrapped fleet aircraft from Fleet list

--- app/Http/Controllers/Fleet/ShowFleetAircraftController.php
+++ app/Http/Controllers/Fleet/ShowFleetAircraftController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Fleet;
 
+use App\Models\Enums\AircraftStatus;
 use App\Models\Fleet;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -21,6 +22,7 @@ class ShowFleetAircraftController extends Controller
         $fleet = Fleet::with(['aircraft' => function ($q) {
             $q->where('is_rental', false);
             $q->where('owner_id', 0);
+            $q->where('status', AircraftStatus::ACTIVE);
             $q->orderBy('hub_id');
         }])
             ->where('company_fleet', true)
