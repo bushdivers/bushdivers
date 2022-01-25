dfa6fcf0680a7ba232f2e3516f3b694040a4e6d8
Russell West <russellwwest@gmail.com>
2022-01-25 16:07:04 +0000

    Adds function to check if a point is within a polygon

---
+++ tests/Unit/Geo/CreatePolygonTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\Geo;
+
+use App\Services\Geo\CreatePolygon;
+use PHPUnit\Framework\TestCase;
+
+class CreatePolygonTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     *
+     */
+    public function test_polygon_has_correct_number_points()
+    {
+        $poly = new CreatePolygon(52.24166, -2.88111, 50);
+        $num = $poly->create();
+        $this->assertEquals(17, count($num));
+    }
+
+    public function test_polygon_has_same_start_and_end()
+    {
+        $poly = new CreatePolygon(52.24166, -2.88111, 50);
+        $polygon = $poly->create();
+        $this->assertEquals($polygon[0][0], $polygon[16][0]);
+    }
+}
---
+++ tests/Unit/Geo/IsPointInPolygonTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Geo;
+
+use App\Services\Geo\CreatePolygon;
+use App\Services\Geo\IsPointInPolygon;
+use PHPUnit\Framework\TestCase;
+
+class IsPointInPolygonTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_egbj_in_50m_radius_egbs()
+    {
+        $p = new CreatePolygon(52.24166, -2.88111, 50);
+        $polygon = $p->create();
+
+        $polyChecker = new IsPointInPolygon();
+        $res = $polyChecker->execute([51.89417, -2.16722], $polygon);
+        $this->assertTrue($res);
+    }
+
+    public function test_lfpg_not_in_50m_radius_egbs()
+    {
+        $p = new CreatePolygon(52.24166, -2.88111, 50);
+        $polygon = $p->create();
+
+        $polyChecker = new IsPointInPolygon();
+        $res = $polyChecker->execute([49.00978, 2.54774], $polygon);
+        $this->assertFalse($res);
+    }
+}
