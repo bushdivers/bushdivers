dd52b7958dbc7ca63c58e6036645e597679133c6
Russell West <russellwwest@gmail.com>
2022-03-29 10:40:20 +0100

    Reduces max number of contracts

--- app/Models/Enums/ContractConsts.php
+++ app/Models/Enums/ContractConsts.php
@@ -6,9 +6,9 @@ class ContractConsts
 {
     public const MIN_QTY_JOBS = 10;
     public const MAX_QTY_JOBS = 30;
-    public const MAX_QTY_JOBS_SM = 15;
-    public const MAX_QTY_JOBS_MD = 22;
-    public const MAX_QTY_JOBS_LG = 30;
+    public const MAX_QTY_JOBS_SM = 8;
+    public const MAX_QTY_JOBS_MD = 16;
+    public const MAX_QTY_JOBS_LG = 20;
     public const PERC_SHORT = 50;
     public const PERC_MED = 25;
     public const PERC_LONG = 25;
