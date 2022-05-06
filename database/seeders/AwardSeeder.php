7347fd20bc1d09d4ad442c401ed66e0f734a743f
Russell West <russellwwest@gmail.com>
2022-05-06 11:55:59 +0100

    update seeders for consistent ids

--- database/seeders/AwardSeeder.php
+++ database/seeders/AwardSeeder.php
@@ -16,18 +16,18 @@ class AwardSeeder extends Seeder
     public function run()
     {
         DB::table('awards')->insert([
-            ['name' => '1 Year Service', 'type' => AwardType::MONTHS, 'value' => 12, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/1Year.png'],
-            ['name' => '6 Months Service', 'type' => AwardType::MONTHS, 'value' => 6, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/6Months.png'],
-            ['name' => '2 Years Service', 'type' => AwardType::MONTHS, 'value' => 24, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/1Year.png'],
-            ['name' => '50 Flights', 'type' => AwardType::FLIGHTS, 'value' => 50, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Flights.png'],
-            ['name' => '50 Hours', 'type' => AwardType::HOURS, 'value' => 50, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Hours.png'],
-            ['name' => '100 Flights', 'type' => AwardType::FLIGHTS, 'value' => 100, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Flights.png'],
-            ['name' => '100 Hours', 'type' => AwardType::HOURS, 'value' => 100, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Hours.png'],
-            ['name' => '200 Flights', 'type' => AwardType::FLIGHTS, 'value' => 200, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Flights.png'],
-            ['name' => '200 Hours', 'type' => AwardType::HOURS, 'value' => 200, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Hours.png'],
-            ['name' => '500 Flights', 'type' => AwardType::FLIGHTS, 'value' => 500, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Flights.png'],
-            ['name' => '500 Hours', 'type' => AwardType::HOURS, 'value' => 500, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/50Hours.png'],
-            ['name' => 'Patreon', 'type' => AwardType::BOOLEAN, 'value' => 1, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/patreon.png']
+            ['id' => 1, 'name' => '1 Year Service', 'type' => AwardType::MONTHS, 'value' => 12, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833876/BDVA/Awards/Year1_wlfbtw.png'],
+            ['id' => 2, 'name' => '6 Months Service', 'type' => AwardType::MONTHS, 'value' => 6, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833876/BDVA/Awards/Months6_qjsz2y.png'],
+            ['id' => 3, 'name' => '2 Years Service', 'type' => AwardType::MONTHS, 'value' => 24, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833876/BDVA/Awards/Years2_sh0xmh.png'],
+            ['id' => 4, 'name' => '50 Flights', 'type' => AwardType::FLIGHTS, 'value' => 50, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833875/BDVA/Awards/Flights50_xfx4at.png'],
+            ['id' => 5, 'name' => '50 Hours', 'type' => AwardType::HOURS, 'value' => 50, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833875/BDVA/Awards/Hours50_qpgbii.png'],
+            ['id' => 6, 'name' => '100 Flights', 'type' => AwardType::FLIGHTS, 'value' => 100, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833875/BDVA/Awards/Flights100_sz5avf.png'],
+            ['id' => 7, 'name' => '100 Hours', 'type' => AwardType::HOURS, 'value' => 100, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833875/BDVA/Awards/Hours100_nzgwp1.png'],
+            ['id' => 8, 'name' => '200 Flights', 'type' => AwardType::FLIGHTS, 'value' => 200, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833875/BDVA/Awards/Flights200_ujrdpb.png'],
+            ['id' => 9, 'name' => '200 Hours', 'type' => AwardType::HOURS, 'value' => 200, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833876/BDVA/Awards/Hours200_bua9ya.png'],
+            ['id' => 10, 'name' => '500 Flights', 'type' => AwardType::FLIGHTS, 'value' => 500, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833875/BDVA/Awards/Flights500_smyz6r.png'],
+            ['id' => 11, 'name' => '500 Hours', 'type' => AwardType::HOURS, 'value' => 500, 'image' => 'https://res.cloudinary.com/dji0yvkef/image/upload/v1651833876/BDVA/Awards/Hours500_rtcrkw.png'],
+            ['id' => 12, 'name' => 'Patreon', 'type' => AwardType::BOOLEAN, 'value' => 1, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630673958/VA/awards/patreon.png']
         ]);
     }
 }
--- database/seeders/RankSeeder.php
+++ database/seeders/RankSeeder.php
@@ -16,11 +16,11 @@ class RankSeeder extends Seeder
     public function run()
     {
         DB::table('ranks')->insert([
-            ['name' => 'Trainee', 'hours' => 0, 'points' => 0, 'pilot_pay' => 25.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/01Trainee.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'Second Officer', 'hours' => 5, 'points' => 50, 'pilot_pay' => 32.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/02SecondOfficer.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'First Officer', 'hours' => 15, 'points' => 150, 'pilot_pay' => 50.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/03FirstOfficer.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'Captain', 'hours' => 45, 'points' => 450, 'pilot_pay' => 75.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/04Captain.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['name' => 'Senior Captain', 'hours' => 135, 'points' => 900, 'pilot_pay' => 100.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/05SeniorCaptain.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 1, 'name' => 'Trainee', 'hours' => 0, 'points' => 0, 'pilot_pay' => 25.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/01Trainee.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 2,'name' => 'Second Officer', 'hours' => 5, 'points' => 50, 'pilot_pay' => 32.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/02SecondOfficer.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 3,'name' => 'First Officer', 'hours' => 15, 'points' => 150, 'pilot_pay' => 50.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/03FirstOfficer.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 4,'name' => 'Captain', 'hours' => 45, 'points' => 450, 'pilot_pay' => 75.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/04Captain.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 5,'name' => 'Senior Captain', 'hours' => 135, 'points' => 900, 'pilot_pay' => 100.00, 'image' => 'https://res.cloudinary.com/dpwytlrc2/image/upload/v1630667098/VA/ranks/05SeniorCaptain.png', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
         ]);
     }
 }
