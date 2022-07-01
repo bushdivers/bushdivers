05968d311e561174f47e1fc5fbcecab097836690
Scott Thomas <s@sthomas.id.au>
2022-07-01 20:07:04 +0930

    Migrate to Vite (#112)

---
+++ resources/js/app.jsx
@@ -0,0 +1,18 @@
+import './bootstrap'
+import '../css/app.css'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+
+InertiaProgress.init()
+
+createInertiaApp({
+  resolve: name => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+  title: title => `${title} - Bush Divers`,
+  setup({ el, App, props }) {
+    render(<App {...props} />, el)
+  }
+})
