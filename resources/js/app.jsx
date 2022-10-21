0b78c6623a07d9fe9db0d08576da4cfa90b0fae9
Russell West <russellwwest@gmail.com>
2022-10-21 09:14:24 +0100

    BD-71 update react

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,7 @@ import './bootstrap'
 import '../css/app.css'
 
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { InertiaProgress } from '@inertiajs/progress'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
@@ -13,6 +13,7 @@ createInertiaApp({
   resolve: name => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
   title: title => `${title} - Bush Divers`,
   setup ({ el, App, props }) {
-    render(<App {...props} />, el)
+    const root = createRoot(el)
+    root.render(<App {...props} />)
   }
 })
