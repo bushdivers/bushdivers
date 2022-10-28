c576548f636cc208823c33d51fc08489bf46e56f
Russell West <russellwwest@gmail.com>
2022-10-28 16:17:12 +0100

    BD-33 initial loan calc page

--- resources/js/Pages/Crew/Loans.jsx
+++ resources/js/Pages/Crew/Loans.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppLayout from '../../Shared/AppLayout'
+import Card from '../../Shared/Elements/Card'
+import TextInput from '../../Shared/Elements/Forms/TextInput'
 
 const Loans = ({ loanValue }) => {
+  const [loanAmount, setLoanAmount] = useState(0)
+  const [errors, setErrors] = useState({
+    loanAmount: null
+  })
+
+  function handleLoanAmountChange (e) {
+    setErrors({ loanAmount: null })
+    const value = e.target.value
+    if (value > loanValue) {
+      setErrors({ loanAmount: 'Value cannot be more than maximum loan value' })
+      return
+    }
+    setLoanAmount(value)
+  }
+
   return (
     <div>
-      Loan amount available: {loanValue}
+      <div className="flex justify-between space-x-4">
+        <div className="w-1/2">
+          <Card title="Calculate Your Loan">
+            <p>Maximum loan amount available to you: <span className="text-lg">${parseFloat(loanValue).toLocaleString()}</span></p>
+            <TextInput id="loanAmount" type="text" value={loanAmount} label="Amount to borrow" error={errors?.loanAmount} onChange={handleLoanAmountChange} />
+          </Card>
+        </div>
+        <div className="w-1/2">
+          <Card title="Your Loan">
+            <h1>${loanAmount > 0 ? <span>{parseFloat(loanAmount).toLocaleString()}</span> : <span>0</span> }</h1>
+          </Card>
+        </div>
+      </div>
     </div>
   )
 }
