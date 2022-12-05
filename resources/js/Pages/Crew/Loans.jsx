f8973a85800ca33196228240c89c124743b298ac
Russell West <russellwwest@gmail.com>
2022-12-05 16:42:58 +0000

    hide input and term if loan not available

--- resources/js/Pages/Crew/Loans.jsx
+++ resources/js/Pages/Crew/Loans.jsx
@@ -62,49 +62,66 @@ const Loans = ({ loanValue, currentLoans }) => {
         <div className="w-full lg:w-1/2">
           <Card title="Calculate Your Loan">
             <p>Maximum loan amount available to you: <span className="text-lg">${parseFloat(loanValue).toLocaleString()}</span></p>
-            <TextInput id="loanAmount" type="text" value={loanAmount} label="Amount to borrow" error={errors?.loanAmount} onChange={handleLoanAmountChange} />
-            <Range id="term" label="Months" value={months} onChange={handleTermChange} min="2" max="12" step="1">
-              <span>2</span>
-              <span>3</span>
-              <span>4</span>
-              <span>5</span>
-              <span>6</span>
-              <span>7</span>
-              <span>8</span>
-              <span>9</span>
-              <span>10</span>
-              <span>11</span>
-              <span>12</span>
-            </Range>
+            {loanValue > 0 && (
+              <>
+                <TextInput id="loanAmount" type="text" value={loanAmount} label="Amount to borrow" error={errors?.loanAmount} onChange={handleLoanAmountChange} />
+                <Range id="term" label="Months" value={months} onChange={handleTermChange} min="2" max="12" step="1">
+                  <span>2</span>
+                  <span>3</span>
+                  <span>4</span>
+                  <span>5</span>
+                  <span>6</span>
+                  <span>7</span>
+                  <span>8</span>
+                  <span>9</span>
+                  <span>10</span>
+                  <span>11</span>
+                  <span>12</span>
+                </Range>
+              </>
+            )
+            }
           </Card>
         </div>
         <div className="w-full lg:w-1/2">
           <Card title="Your Monthly Loan">
-            <div className="flex justify-between">
-              <div className="text-4xl">${parseFloat(payment).toLocaleString(navigator.language, { maximumFractionDigits: 0 })}</div>
-              <img className="h-32" src="https://res.cloudinary.com/dji0yvkef/image/upload/v1669234939/BDVA/Hornbill_Financing_logo2_vgcqdc.png" alt="hornbilfinance" />
-            </div>
-            <div className="flex justify-start mt-6 space-x-4">
-              <div>
-                <div className="stat-title">Loan Amount</div>
-                <div className="text-2xl">${loanAmount > 0 ? <span>{loanAmount.toLocaleString(navigator.language, { maximumFractionDigits: 0 })}</span> : <span>0</span> }</div>
-                <div className="stat-desc">The amount to borrow</div>
-              </div>
-              <div>
-                <div className="stat-title">Term</div>
-                <div className="text-2xl">{months} months</div>
-                <div className="stat-desc">Number of months</div>
-              </div>
-              <div>
-                <div className="stat-title">Total Cost of Loan</div>
-                <div className="text-2xl">${total.toLocaleString(navigator.language, { maximumFractionDigits: 0 })}</div>
-                <div className="stat-desc">The loan amount plus interest</div>
-              </div>
-            </div>
-            <div className="flex justify-end mt-4">
-              <button onClick={() => applyForLoan()} className="btn btn-primary">Apply for Loan</button>
-            </div>
-            {applyError && <span className="text-sm text-error">{applyError}</span>}
+            {loanValue > 0
+              ? (
+                <>
+                  <div className="flex justify-between">
+                    <div className="text-4xl">${parseFloat(payment).toLocaleString(navigator.language, { maximumFractionDigits: 0 })}</div>
+                    <img className="h-32" src="https://res.cloudinary.com/dji0yvkef/image/upload/v1669234939/BDVA/Hornbill_Financing_logo2_vgcqdc.png" alt="hornbilfinance" />
+                  </div>
+                  <div className="flex justify-start mt-6 space-x-4">
+                    <div>
+                      <div className="stat-title">Loan Amount</div>
+                      <div className="text-2xl">${loanAmount > 0 ? <span>{loanAmount.toLocaleString(navigator.language, { maximumFractionDigits: 0 })}</span> : <span>0</span> }</div>
+                      <div className="stat-desc">The amount to borrow</div>
+                    </div>
+                    <div>
+                      <div className="stat-title">Term</div>
+                      <div className="text-2xl">{months} months</div>
+                      <div className="stat-desc">Number of months</div>
+                    </div>
+                    <div>
+                      <div className="stat-title">Total Cost of Loan</div>
+                      <div className="text-2xl">${total.toLocaleString(navigator.language, { maximumFractionDigits: 0 })}</div>
+                      <div className="stat-desc">The loan amount plus interest</div>
+                    </div>
+                  </div>
+                  <div className="flex justify-end mt-4">
+                    <button onClick={() => applyForLoan()} className="btn btn-primary">Apply for Loan</button>
+                  </div>
+                  {applyError && <span className="text-sm text-error">{applyError}</span>}
+                </>
+                )
+              : (
+                <div className="flex justify-between">
+                  <div className="text-xl">Unfortunately you cannot use Hornbill Financing at this point in time.</div>
+                  <img className="h-32" src="https://res.cloudinary.com/dji0yvkef/image/upload/v1669234939/BDVA/Hornbill_Financing_logo2_vgcqdc.png" alt="hornbilfinance" />
+                </div>
+                )
+            }
           </Card>
         </div>
       </div>
