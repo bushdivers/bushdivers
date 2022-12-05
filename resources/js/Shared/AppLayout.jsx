e8e58d75c279f4db4697bcff9a0f65c73a1e3f88
Russell West <russellwwest@gmail.com>
2022-12-05 12:02:56 +0000

    fixes unused import

--- resources/js/Shared/AppLayout.jsx
+++ resources/js/Shared/AppLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Head, Link, usePage } from '@inertiajs/inertia-react'
 import FlashMessage from './Elements/FlashMessage'
 import { Toaster } from 'react-hot-toast'
