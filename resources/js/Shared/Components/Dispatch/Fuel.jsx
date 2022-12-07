3ea2e4e58d5ecbcdb0b813dc8dcbb5894bde496b
Russell West <russellwwest@gmail.com>
2022-12-07 14:51:52 +0000

    Update Fuel.jsx

--- resources/js/Shared/Components/Dispatch/Fuel.jsx
+++ resources/js/Shared/Components/Dispatch/Fuel.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../../Elements/Card'
 import TextInput from '../../Elements/Forms/TextInput'
 
 const Fuel = (props) => {
+  const [fuelWeight, setFuelWeight] = useState(null)
+  useEffect(() => {
+    if (props.selectedAircraft) {
+      const weight = props.selectedAircraft.fleet.fuel_type_id === 1 ? props.fuel * 5.99 : props.fuel * 6.79
+      setFuelWeight(weight)
+    }
+  }, [props.fuel])
+
   return (
     <div className="mt-2 mr-2">
       <Card title="Select Fuel">
@@ -13,6 +21,7 @@ const Fuel = (props) => {
             Current Fuel (gal):
             <div className="w-full lg:w-1/4">
               <TextInput id="fuel" type="text" value={props.fuel} onChange={props.handleUpdateFuel} error={props.error} />
+              <div className="text-sm">{fuelWeight} lbs <span className="italic">(estimated)</span></div>
             </div>
           </div>
         </div>
