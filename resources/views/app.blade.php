736db4364985df09c124e16754459680d1e78c4e
Russell West <russellwwest@gmail.com>
2023-01-31 15:23:10 +0000

    adds google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,6 +21,15 @@
         window.global = window
     </script>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-74R06CETYB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-74R06CETYB');
+</script>
 <body>
     @inertia
 </body>
