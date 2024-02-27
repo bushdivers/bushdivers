f48e2f6bb95bb6134f8d0f48a2dd0c6486128e64
Russell West <russellwwest@gmail.com>
2024-02-27 22:33:32 +0000

    fixes silly admin menu missing bug
    I thought I was being clever :)

--- resources/js/components/layout/navigation/sidebar/NavMenuItems.jsx
+++ resources/js/components/layout/navigation/sidebar/NavMenuItems.jsx
@@ -43,10 +43,12 @@ const NavMenuItems = () => {
       </Box>
       <Divider mt={4} />
       <Box my={4}>
-        {!!auth.user.is_admin ||
-          (doesUserHaveRole(auth.user.user_roles, 'fleet_admin') && (
-            <NavItem icon={Cog} text="Admin" to="/admin/fleet" />
-          ))}
+        {auth.user.is_admin ||
+        doesUserHaveRole(auth.user.user_roles, 'fleet_admin') ? (
+          <NavItem icon={Cog} text="Admin" to="/admin/fleet" />
+        ) : (
+          <></>
+        )}
         <NavItem icon={HelpCircle} text="Help (Coming Soon)" to="" />
         <NavItem
           isExternal
