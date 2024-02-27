a12df7ccd1048e57659bfffe96a19f43521a9728
Russell West <russellwwest@gmail.com>
2024-02-27 22:07:44 +0000

    show admin menu if admin or have fleet admin role

--- resources/js/components/layout/navigation/sidebar/NavMenuItems.jsx
+++ resources/js/components/layout/navigation/sidebar/NavMenuItems.jsx
@@ -15,14 +15,12 @@ import {
 } from 'lucide-react'
 import React from 'react'
 
+import { doesUserHaveRole } from '../../../../helpers/auth.helpers.js'
 import NavItem from './NavItem.jsx'
 
 const NavMenuItems = () => {
   const { auth } = usePage().props
-  const isFleetAdmin = () => {
-    console.log(auth.user.user_roles)
-    return auth.user.user_roles.includes('fleet_admin')
-  }
+
   return (
     <Box>
       <Box my={4}>
@@ -45,11 +43,10 @@ const NavMenuItems = () => {
       </Box>
       <Divider mt={4} />
       <Box my={4}>
-        {auth.user.is_admin || isFleetAdmin() ? (
-          <NavItem icon={Cog} text="Admin" to="/admin/fleet" />
-        ) : (
-          <></>
-        )}
+        {!!auth.user.is_admin ||
+          (doesUserHaveRole(auth.user.user_roles, 'fleet_admin') && (
+            <NavItem icon={Cog} text="Admin" to="/admin/fleet" />
+          ))}
         <NavItem icon={HelpCircle} text="Help (Coming Soon)" to="" />
         <NavItem
           isExternal
