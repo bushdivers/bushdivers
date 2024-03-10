5fc265f3455200fc0dde6135de608d29e30e977d
Russell West <russellwwest@gmail.com>
2024-03-10 21:54:03 +0000

    adds fuel in migration

--- database/migrations/2024_03_05_224949_adds_fuel_to_airports_table.php
+++ database/migrations/2024_03_05_224949_adds_fuel_to_airports_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,10 +14,35 @@ return new class extends Migration
     {
         Schema::table('airports', function (Blueprint $table) {
             $table->bigInteger('avgas_qty')->nullable()->default(0);
-            $table->decimal('avgas_price', 12, 2)->nullable()->default(3.10);
+            $table->decimal('avgas_price', 12, 2)->nullable()->default(6.10);
             $table->bigInteger('jetfuel_qty')->nullable()->default(0);
-            $table->decimal('jetfuel_price', 12, 2)->nullable()->default(2.60);
+            $table->decimal('jetfuel_price', 12, 2)->nullable()->default(4.60);
         });
+
+        // ensure hubs have avgas and jetful unlimited
+        DB::update(
+    'UPDATE airports SET has_avgas = true, has_jetfuel = true, avgas_qty = null, jetfuel_qty = null WHERE is_hub = true'
+        );
+
+        // size 4+ random qty
+        DB::update(
+            'UPDATE airports SET avgas_qty = ? WHERE size >= 4 AND is_hub = false AND has_avgas = true',
+            [mt_rand(5000, 10000)]
+        );
+        DB::update(
+            'UPDATE airports SET jetfuel_qty = ? WHERE size >= 4 AND is_hub = false AND has_jetfuel = true',
+            [mt_rand(5000, 10000)]
+        );
+
+        // size 3 + lower
+        DB::update(
+            'UPDATE airports SET avgas_qty = ? WHERE size <= 3 AND is_hub = false AND has_avgas = true',
+            [mt_rand(500, 1000)]
+        );
+        DB::update(
+            'UPDATE airports SET jetfuel_qty = ? WHERE size <= 3 AND is_hub = false AND has_jetfuel = true',
+            [mt_rand(500, 1000)]
+        );
     }
 
     /**
