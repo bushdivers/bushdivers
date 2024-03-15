326f5eec92a6f5a29c9f8698ccf25d192f3cafdd
Russell West <russellwwest@gmail.com>
2024-03-15 17:31:51 +0000

    updates base payloads

--- app/Services/Contracts/GenerateContractDetails.php
+++ app/Services/Contracts/GenerateContractDetails.php
@@ -65,43 +65,6 @@ class GenerateContractDetails
                 ];
             return $contract;
 
-
-//            foreach ($airports as $airport) {
-//                if ($origin->identifier != $airport->identifier) {
-//                    // generate cargo
-//                    $cargo = $this->generateContractCargo->execute();
-//                    // get distance and heading
-//                    $distance = $this->calcDistanceBetweenPoints->execute($origin->lat, $origin->lon, $airport->lat, $airport->lon);
-//                    $heading = $this->calcBearingBetweenPoints->execute($origin->lat, $origin->lon, $airport->lat, $airport->lon, $airport->magnetic_variance);
-//                    $expiry = Carbon::now()->addDays(rand(1,8));
-//
-//                    $expiryMultiplier = match (true) {
-//                        $expiry > Carbon::now()->addDays(5) && $expiry < Carbon::now()->addDays(7) => 1.2,
-//                        $expiry > Carbon::now()->addDays(3) && $expiry < Carbon::now()->addDays(5) => 1.5,
-//                        $expiry > Carbon::now()->addDays(1) && $expiry < Carbon::now()->addDays(3) => 1.8,
-//                        $expiry < Carbon::now()->addHours(24) => 2.0,
-//                        default => 1.0,
-//                    };
-//
-//                    $contractValue = $this->calcContractValue->execute($cargo['type'], $cargo['qty'], $distance);
-//                    $contractValue = $contractValue * $expiryMultiplier;
-//                    // create contract
-//                    $contract = [
-//                        'id' => $origin->identifier.'-'.$airport->identifier,
-//                        'departure' => $origin->identifier,
-//                        'destination' => $airport->identifier,
-//                        'cargo' => $cargo['name'],
-//                        'cargo_type' => $cargo['type'],
-//                        'cargo_qty' => $cargo['qty'],
-//                        'distance' => $distance,
-//                        'heading' => $heading,
-//                        'contract_value' => $contractValue,
-//                        'expires_at' => $expiry
-//                    ];
-//                    $contracts[] = $contract;
-//                }
-//            }
-//            return $contracts;
         } catch (\Exception $e) {
             Log::channel('single')->debug($e->getMessage(), ['where' => 'Contract details generation']);
         }
