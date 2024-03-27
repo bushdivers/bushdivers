38a6c66ac1e67525efccd6a4546de9479219f491
Russell West <russellwwest@gmail.com>
2024-03-27 22:18:56 +0000

    skew random cargo qty to be a bit smaller

--- app/Services/Contracts/GenerateContractCargo.php
+++ app/Services/Contracts/GenerateContractCargo.php
@@ -16,10 +16,18 @@ class GenerateContractCargo
         $types = DB::table('cargo_types')->get();
         $cargo = $types->random();
         if ($cargo->type == 1) {
-            $qty = rand($minCargo, $maxCargo);
+            $qty = $this->skewedRandom($minCargo, $maxCargo);
         } else {
-            $qty = rand($minPax, $maxPax);
+            $qty = $this->skewedRandom($minPax, $maxPax);
         }
         return ['name' => $cargo->text, 'type' => $cargo->type, 'qty' => $qty];
+    }
+
+    protected function skewedRandom($min, $max): float|object|int
+    {
+        // Adjust the power value to control the skewness
+        $skew = 1.8;
+        $offset = $max - $min + 1;
+        return floor($min + pow(lcg_value(), $skew) * $offset);
     }
 }
