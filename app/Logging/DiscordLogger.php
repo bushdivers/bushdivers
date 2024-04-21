b0fc76f581e666eb9b3095131c2f0b3b8fc25eea
Scott Thomas <s@sthomas.id.au>
2024-04-21 15:12:35 +0930

    Fix logging to Discord if message exceeds max length

--- app/Logging/DiscordLogger.php
+++ app/Logging/DiscordLogger.php
@@ -3,6 +3,7 @@
 namespace App\Logging;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\LogRecord;
 
@@ -24,9 +25,16 @@ class DiscordLogger extends AbstractProcessingHandler {
 
         // Trim result
         $message = implode("\n", array_slice(explode("\n", $record->formatted, 8), 0, 7));
+        $message = str_replace('`', '', $message);
+        $message = "**An error occurred**\n\n```$message```";
+        // 2000 character cap
+        $message = substr($message, 0, 2000);
 
-        Http::post($this->webhookUrl, [
-            'content' => "**An error occurred**\n\n```$message```"
+        $response = Http::post($this->webhookUrl, [
+            'content' => $message,
         ]);
+
+        if ($response->status() != 200)
+            Log::channel('single')->error('DiscordLogger: Failed to send message to Discord', ['response' => $response->body()]);
     }
 }
