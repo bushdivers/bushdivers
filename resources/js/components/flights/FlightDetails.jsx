a18ef251a2215331fa72612838287df5c7352046
Russell West <russellwwest@gmail.com>
2024-04-30 19:53:38 +0100

    fix overflow issue on live flights

--- resources/js/components/flights/FlightDetails.jsx
+++ resources/js/components/flights/FlightDetails.jsx
@@ -87,8 +87,8 @@ const FlightDetails = ({ flights }) => {
   }, [selectedFlight])
 
   return (
-    <Box position="absolute" left={4} top={12} bottom={12} w={300}>
-      <Card overflowY="auto">
+    <Card position="absolute" left={4} top={12} bottom={12} w={300} h="100%">
+      <Box p={2} overflowY="auto">
         <Flex mt={2} px={2} alignItems="center" justifyContent="space-between">
           <Heading size="sm">
             {selectedFlight
@@ -104,167 +104,151 @@ const FlightDetails = ({ flights }) => {
             </Button>
           )}
         </Flex>
-        <Box p={2} overflowY="auto">
-          {selectedFlight && (
-            <Box>
-              <Flex mt={2} justifyContent="center" alignItems="center" gap={3}>
-                <Flex alignItems="center" direction="column">
-                  <Heading size="md">
-                    {selectedFlight.dep_airport.identifier}
-                  </Heading>
-                  <Text fontSize="xs">{selectedFlight.dep_airport.name}</Text>
-                </Flex>
-                <Icon boxSize={6} as={Plane} />
-                <Flex alignItems="center" direction="column">
-                  <Heading size="md">
-                    {selectedFlight.arr_airport.identifier}
-                  </Heading>
-                  <Text fontSize="xs">{selectedFlight.arr_airport.name}</Text>
-                </Flex>
+        {selectedFlight && (
+          <Box>
+            <Flex mt={2} justifyContent="center" alignItems="center" gap={3}>
+              <Flex alignItems="center" direction="column">
+                <Heading size="md">
+                  {selectedFlight.dep_airport.identifier}
+                </Heading>
+                <Text fontSize="xs">{selectedFlight.dep_airport.name}</Text>
               </Flex>
-              <Progress
-                size="sm"
-                mt={2}
-                hasStripe
-                value={calcFlightProgress()}
-              />
-              <Flex justifyContent="space-between" alignItems="center">
+              <Icon boxSize={6} as={Plane} />
+              <Flex alignItems="center" direction="column">
+                <Heading size="md">
+                  {selectedFlight.arr_airport.identifier}
+                </Heading>
+                <Text fontSize="xs">{selectedFlight.arr_airport.name}</Text>
+              </Flex>
+            </Flex>
+            <Progress size="sm" mt={2} hasStripe value={calcFlightProgress()} />
+            <Flex justifyContent="space-between" alignItems="center">
+              <Text fontSize="xs">
+                {getDistance(
+                  selectedFlight.current_lat,
+                  selectedFlight.current_lon,
+                  selectedFlight.dep_airport.lat,
+                  selectedFlight.dep_airport.lon
+                )}{' '}
+                nm
+              </Text>
+              <Flex alignItems="center" gap={1}>
+                <Text fontSize="xs">ETA {calcFlightETA()}</Text>
                 <Text fontSize="xs">
                   {getDistance(
                     selectedFlight.current_lat,
                     selectedFlight.current_lon,
-                    selectedFlight.dep_airport.lat,
-                    selectedFlight.dep_airport.lon
+                    selectedFlight.arr_airport.lat,
+                    selectedFlight.arr_airport.lon
                   )}{' '}
                   nm
                 </Text>
-                <Flex alignItems="center" gap={1}>
-                  <Text fontSize="xs">ETA {calcFlightETA()}</Text>
-                  <Text fontSize="xs">
-                    {getDistance(
-                      selectedFlight.current_lat,
-                      selectedFlight.current_lon,
-                      selectedFlight.arr_airport.lat,
-                      selectedFlight.arr_airport.lon
-                    )}{' '}
-                    nm
-                  </Text>
+              </Flex>
+            </Flex>
+            <Flex mt={1} justifyContent="center" alignItems="center" gap={2}>
+              <Heading textAlign="center" size="xs">
+                {selectedFlight.pilot.pilot_id}
+              </Heading>
+              <Text fontSize="xs" textAlign="center">
+                {selectedFlight.pilot.discord_username
+                  ? selectedFlight.pilot.discord_username
+                  : selectedFlight.pilot.private_name}
+              </Text>
+            </Flex>
+            <Divider mt={2} />
+            <Flex mt={2} alignItems="center" gap={1}>
+              <Heading size="xs">Aircraft Type</Heading>
+              <Text fontSize="xs">
+                (
+                {selectedFlight.is_rental
+                  ? selectedFlight.rental.fleet.type
+                  : selectedFlight.aircraft.fleet.type}
+                )
+              </Text>
+            </Flex>
+            <Flex justifyContent="space-between">
+              <Text fontSize="sm">
+                {selectedFlight.is_rental
+                  ? selectedFlight.rental.fleet.manufacturer +
+                    ' ' +
+                    selectedFlight.rental.fleet.name
+                  : selectedFlight.aircraft.fleet.manufacturer +
+                    ' ' +
+                    selectedFlight.aircraft.fleet.name}
+              </Text>
+              <Text fontSize="sm">
+                {selectedFlight.is_rental
+                  ? selectedFlight.rental.registration
+                  : selectedFlight.aircraft.registration}
+              </Text>
+            </Flex>
+            <Divider mt={2} />
+            <Flex mt={2} justifyContent="space-between" alignItems="center">
+              <Flex direction="column">
+                <Heading size="xs">Altitude</Heading>
+                <Text fontSize="xs">{selectedFlight.current_altitude} ft</Text>
+              </Flex>
+              <Flex direction="column">
+                <Heading size="xs">IAS</Heading>
+                <Text fontSize="xs">
+                  {selectedFlight.current_indicated_speed} kts
+                </Text>
+              </Flex>
+              <Flex direction="column">
+                <Heading size="xs">Track</Heading>
+                <Text fontSize="xs">{selectedFlight.current_heading}&deg;</Text>
+              </Flex>
+            </Flex>
+          </Box>
+        )}
+        {!selectedFlight &&
+          flights &&
+          flights.length > 0 &&
+          flights.map((flight) => (
+            <Card
+              my={1}
+              cursor="pointer"
+              p={2}
+              key={flight.id}
+              onClick={() =>
+                setSelectedFlight(
+                  flight.id === selectedFlight?.id ? null : flight
+                )
+              }
+            >
+              <Flex mt={2} justifyContent="center" alignItems="center" gap={3}>
+                <Flex alignItems="center" direction="column">
+                  <Heading size="md">{flight.dep_airport.identifier}</Heading>
+                  <Text fontSize="xs">{flight.dep_airport.name}</Text>
+                </Flex>
+                <Icon boxSize={6} as={Plane} />
+                <Flex alignItems="center" direction="column">
+                  <Heading size="md">{flight.arr_airport.identifier}</Heading>
+                  <Text fontSize="xs">{flight.arr_airport.name}</Text>
                 </Flex>
               </Flex>
-              <Flex mt={1} justifyContent="center" alignItems="center" gap={2}>
+              <Flex justifyContent="center" alignItems="center" gap={2}>
                 <Heading textAlign="center" size="xs">
-                  {selectedFlight.pilot.pilot_id}
+                  {flight.pilot.pilot_id}
                 </Heading>
                 <Text fontSize="xs" textAlign="center">
-                  {selectedFlight.pilot.discord_username
-                    ? selectedFlight.pilot.discord_username
-                    : selectedFlight.pilot.private_name}
+                  {flight.pilot.discord_username
+                    ? flight.pilot.discord_username
+                    : flight.pilot.private_name}
                 </Text>
               </Flex>
-              <Divider mt={2} />
-              <Flex mt={2} alignItems="center" gap={1}>
-                <Heading size="xs">Aircraft Type</Heading>
-                <Text fontSize="xs">
-                  (
-                  {selectedFlight.is_rental
-                    ? selectedFlight.rental.fleet.type
-                    : selectedFlight.aircraft.fleet.type}
-                  )
-                </Text>
-              </Flex>
-              <Flex justifyContent="space-between">
-                <Text fontSize="sm">
-                  {selectedFlight.is_rental
-                    ? selectedFlight.rental.fleet.manufacturer +
-                      ' ' +
-                      selectedFlight.rental.fleet.name
-                    : selectedFlight.aircraft.fleet.manufacturer +
-                      ' ' +
-                      selectedFlight.aircraft.fleet.name}
-                </Text>
-                <Text fontSize="sm">
-                  {selectedFlight.is_rental
-                    ? selectedFlight.rental.registration
-                    : selectedFlight.aircraft.registration}
-                </Text>
-              </Flex>
-              <Divider mt={2} />
-              <Flex mt={2} justifyContent="space-between" alignItems="center">
-                <Flex direction="column">
-                  <Heading size="xs">Altitude</Heading>
-                  <Text fontSize="xs">
-                    {selectedFlight.current_altitude} ft
-                  </Text>
-                </Flex>
-                <Flex direction="column">
-                  <Heading size="xs">IAS</Heading>
-                  <Text fontSize="xs">
-                    {selectedFlight.current_indicated_speed} kts
-                  </Text>
-                </Flex>
-                <Flex direction="column">
-                  <Heading size="xs">Track</Heading>
-                  <Text fontSize="xs">
-                    {selectedFlight.current_heading}&deg;
-                  </Text>
-                </Flex>
-              </Flex>
-            </Box>
-          )}
-          {!selectedFlight &&
-            flights &&
-            flights.length > 0 &&
-            flights.map((flight) => (
-              <Card
-                my={1}
-                cursor="pointer"
-                p={2}
-                key={flight.id}
-                onClick={() =>
-                  setSelectedFlight(
-                    flight.id === selectedFlight?.id ? null : flight
-                  )
-                }
-              >
-                <Flex
-                  mt={2}
-                  justifyContent="center"
-                  alignItems="center"
-                  gap={3}
+              <Flex justifyContent="end">
+                <Tag
+                  size="sm"
+                  colorScheme={flight.status === 0 ? 'gray' : 'orange'}
                 >
-                  <Flex alignItems="center" direction="column">
-                    <Heading size="md">{flight.dep_airport.identifier}</Heading>
-                    <Text fontSize="xs">{flight.dep_airport.name}</Text>
-                  </Flex>
-                  <Icon boxSize={6} as={Plane} />
-                  <Flex alignItems="center" direction="column">
-                    <Heading size="md">{flight.arr_airport.identifier}</Heading>
-                    <Text fontSize="xs">{flight.arr_airport.name}</Text>
-                  </Flex>
-                </Flex>
-                <Flex justifyContent="center" alignItems="center" gap={2}>
-                  <Heading textAlign="center" size="xs">
-                    {flight.pilot.pilot_id}
-                  </Heading>
-                  <Text fontSize="xs" textAlign="center">
-                    {flight.pilot.discord_username
-                      ? flight.pilot.discord_username
-                      : flight.pilot.private_name}
-                  </Text>
-                </Flex>
-                <Flex justifyContent="end">
-                  <Tag
-                    size="sm"
-                    colorScheme={flight.status === 0 ? 'gray' : 'orange'}
-                  >
-                    {renderStatus(flight.status)}
-                  </Tag>
-                </Flex>
-              </Card>
-            ))}
-        </Box>
-      </Card>
-    </Box>
+                  {renderStatus(flight.status)}
+                </Tag>
+              </Flex>
+            </Card>
+          ))}
+      </Box>
+    </Card>
   )
 }
 
