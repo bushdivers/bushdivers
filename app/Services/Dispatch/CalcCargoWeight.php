e3107523f5e6b1ae6d426f250358b20176947372
Scott Thomas <s@sthomas.id.au>
2026-04-13 22:16:12 +0930

    Fix active dispatch preview miscalculating cargo weights

--- app/Services/Dispatch/CalcCargoWeight.php
+++ app/Services/Dispatch/CalcCargoWeight.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Dispatch;
 
+use App\Models\Enums\CargoType;
 use App\Models\Enums\WeightConsts;
 
 class CalcCargoWeight
@@ -11,11 +12,11 @@ class CalcCargoWeight
         $total = 0;
         foreach ($cargo as $c) {
             if (!$calcPax) {
-                if ($c->cargo_type == 1) {
+                if ($c->cargo_type == 1 || $c->cargo_type == CargoType::Cargo) {
                     $total += $c->cargo_qty;
                 }
             } else {
-                if ($c->cargo_type == 1) {
+                if ($c->cargo_type == 1 || $c->cargo_type == CargoType::Cargo) {
                     $total += $c->cargo_qty;
                 } else {
                     $total += ($c->cargo_qty * WeightConsts::PERSON_WEIGHT);
