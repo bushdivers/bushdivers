eaa5eca148a9822d142d1cbb94596eb66f0e03a2
Scott Thomas <s@sthomas.id.au>
2026-04-26 14:27:59 +0930

    Fix repeated toasts not displaying

--- resources/js/components/layout/FlashSection.jsx
+++ resources/js/components/layout/FlashSection.jsx
@@ -1,31 +1,31 @@
 import { useToast } from '@chakra-ui/react'
-import { usePage } from '@inertiajs/react'
+import { router } from '@inertiajs/react'
 import { useEffect } from 'react'
 
 const FlashSection = () => {
-  const { flash } = usePage().props
   const toast = useToast()
 
   useEffect(() => {
-    if (flash.success) {
-      toast({
-        title: flash.success,
-        status: 'success',
-        duration: 5000,
-        isClosable: true,
-        position: 'top-right',
-      })
-    }
-    if (flash.error) {
-      toast({
-        title: flash.error,
-        status: 'error',
-        duration: 5000,
-        isClosable: true,
-        position: 'top-right',
-      })
-    }
-  }, [flash.success, flash.error])
+    return router.on('success', (event) => {
+      const { flash } = event.detail.page.props
+      if (flash?.success) {
+        toast({
+          title: flash.success,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        })
+      }
+      if (flash?.error) {
+        toast({
+          title: flash.error,
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        })
+      }
+    })
+  }, [])
 
   return null
 }
